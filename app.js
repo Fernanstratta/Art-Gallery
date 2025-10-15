90ee386bb6db1293251ea28ff6f0e68b3cf75848
ClydeSeven <angel.magdaleno@cetys.edu.mx>
2025-10-15 14:51:05 -0700

    sharingan

--- app.js
+++ app.js
@@ -18,47 +18,82 @@ function dibujar() {
 }
 
 function dib0() {
-
-const c = document.getElementById('canva1');
-
+  const c = document.getElementById('canva1');
   c.width = c.clientWidth;
   c.height = c.clientHeight;
-
   const ctx = c.getContext("2d");
-  ctx.fillStyle = '#ffffffff';
-  ctx.fillRect(0, 0, c.width, c.height); 
 
-  ctx.fillStyle = 'black';
+  let rotation = 0; 
+
+  function drawSymbol(angle) {
+    ctx.save();
+    ctx.translate(150, 125); 
+    ctx.rotate(angle);
+
+ 
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(38, 0, 10, 0, 1.25 * Math.PI, 1);
+    ctx.fill();
+    ctx.closePath();
+
+
+    ctx.beginPath();
+    ctx.arc(38, -5, 7, 0, 2 * Math.PI);
+    ctx.fill();
+
+    ctx.restore();
+  }
+
+  function animate() {
+    ctx.clearRect(0, 0, c.width, c.height);
+
+
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(0, 0, c.width, c.height);
+
+
+    ctx.fillStyle = 'black';
     ctx.font = '18px sans-serif';
-    ctx.fillText('wow', 15, 30);
-  ///
-        ctx.fillStyle = "red";
-        ctx.beginPath();
-        ctx.arc(150, 125, 55, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.fill();
+    ctx.fillText('Sharingan', 15, 30);
 
 
-        ctx.fillStyle = "black";
-        ctx.beginPath();
-        ctx.arc(150, 125, 40, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.fill();    
-        
-        ctx.fillStyle = "red";
-        ctx.beginPath();
-        ctx.arc(150, 125, 38, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.fill(); 
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(150, 125, 55, 0, 2 * Math.PI);
+    ctx.fill();
 
 
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(150, 125, 40, 0, 2 * Math.PI);
+    ctx.fill();
 
-        ctx.fillStyle = "black";
-        ctx.beginPath();
-        ctx.arc(150, 125, 12, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.fill();
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(150, 125, 38, 0, 2 * Math.PI);
+    ctx.fill();
+
+
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(150, 125, 12, 0, 2 * Math.PI);
+    ctx.fill();
+
+ 
+    for (let i = 0; i < 3; i++) {
+      drawSymbol(rotation + (i * (2 * Math.PI / 3))); 
+    }
+
+
+    rotation += 0.02;
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
 }
+
 
 function dib1() {
   const c = document.getElementById('canva2');
